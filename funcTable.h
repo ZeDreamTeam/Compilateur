d1682f8579d3d045bdb3406a231bc585e4f6db46
Mickael Goubin <micky0304@gmail.com>
2015-05-31 19:46:36 +0200

    comments...

--- funcTable.h
+++ funcTable.h
@@ -1,9 +1,23 @@
 #ifndef FUNC_TABLE
 #define FUNC_TABLE
+/*
+ * Gestion de la table des fonctions
+ */
 
+
+/*
+ * Ajoute un element a la table des fonctions
+ */
 void ftable_add(char* name, int nbArgs, int line);
 
+/*
+ * Verifie si une fonction de ce nom existe ou pas
+ */
 int ftable_exists(char * name, int nbArgs);
+
+/*
+ * Affiche la table des symboles.
+ */
 void ftable_print();
 
 #endif
