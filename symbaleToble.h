83da46b9df5386dd267e5d1a4092ca5cc8642e3d
Mickael Goubin <micky0304@gmail.com>
2015-05-31 19:41:07 +0200

    Add intel on symbole table

--- symbaleToble.h
+++ symbaleToble.h
@@ -1,5 +1,9 @@
 #ifndef omg
 #define omg
+/*
+ * Fichier utilisé pour la gestion de la table des symboles.
+ */
+
 
 typedef struct {
     int address;
@@ -7,15 +11,55 @@ typedef struct {
     int isConst;
     int isInit;
 } Symbale;
+
+/*
+ * Affichage de la table des symboles
+ */
 void printTableSymboleST();
-void yyerror(char const *err);
+
+/*
+ * Defini si un symbole à l'addresse index est constant ou pas.
+ */
 void setIfSymboleIsConstST(int index, int isConst);
+/*
+ * Ajoute un symbole dans la table
+ */
 int symbolePushST(char* name, int isConst, int isInit);
+/*
+ * Enleve le dernier symboel ajoutée à notre table.
+ */
 void symbolePopST();
+
+/*
+ * Défini qu'un symbole est initialisé.
+ */
 void setIsInitST(int index);
+
+/*
+ * Défini qu'un symbole est initialisé depuis le nom de ce symbole.
+ */
 void symbolInitST(char* name);
+
+/*
+ * Enleve les derniers symbole dans la table jusqu'à atteindre l'addresse adress.
+ */ 
 void popTilST(int adress);
+
+/*
+ * Recupere l'index d'une variable depuis son nom
+ */
 int getIndexWithVarNameST(char* name);
+
+/*
+ * Ajout d'une variable temporaire (nom nommée)
+ */
 int tempAddST();
+
+/*
+ * Libere l'adresse de la derniere variable temporaire
+ */
 int tempPopST();
+
+/* retourne une erreur yacc.*/
+void yyerror(char const *err);
 #endif
